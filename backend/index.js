2805f4aef2c3f9529d195179ddaf6d8d3460ff1f
Abhiram Yadav M <121328052+AbhiramYad@users.noreply.github.com>
2026-02-08 16:49:40 +0530

    Update CORS origin to include production URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://ecommerceone-h8gi.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
